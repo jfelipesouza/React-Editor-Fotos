7499547e5d1851dae9db3fc1c45b88a54e4afc86
Felipe Souza <j.felipe.dev.18@gmail.com>
2021-10-27 00:10:36 -0300

    Arrumando o codigo da funçao de download

--- src/components/DownloadImageButton/downloadFuction.ts
+++ src/components/DownloadImageButton/downloadFuction.ts
@@ -1,43 +1,44 @@
 import domtoimage from "dom-to-image";
 
 function downloadImage() {
+  var node = document.getElementById("img") as HTMLElement;
 
-    var node = document.getElementById('img') as HTMLElement;
+  const scale = 3;
 
-    const scale = 3;
+  const style = {
+    transform: "scale(" + scale + ")",
+    transformOrigin: "top left",
+    width: node.offsetWidth + "px",
+    height: node.offsetHeight + "px",
+  };
 
-    const style = {
-        transform: 'scale('+scale+')',
-        transformOrigin: 'top left',
-        width: node.offsetWidth + "px",
-        height: node.offsetHeight + "px"
-    }
+  const param = {
+    height: node.offsetHeight * scale,
+    width: node.offsetWidth * scale,
+    quality: 1,
+    style,
+  };
 
-    const param = {
-        height: node.offsetHeight * scale,
-        width: node.offsetWidth * scale,
-        quality: 1,
-        style
-    }
-
-    domtoimage.toPng(node, param).then(function (dataUrl) {
-            var img = new Image();
-            img.src = dataUrl;
-            downloadURI(dataUrl, "image.png")
-        })
-        .catch(function (error) {
-            console.error('Deu ruim, se fudeu!', error);
-        });
+  domtoimage
+    .toPng(node, param)
+    .then(function (dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      downloadURI(dataUrl, "image.png");
+    })
+    .catch(function (error) {
+      console.error("Deu ruim, se fudeu!", error);
+    });
 }
 
-function downloadURI(uri:any, name:any) {
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    link.remove();
+function downloadURI(uri: any, name: any) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  link.remove();
 }
 
 export default downloadImage;
